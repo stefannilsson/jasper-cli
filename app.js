f21ca35fb437fa093c701444f96c74371539b394
Stefan Nilsson <stefan@stefannilssonconsulting.com>
2020-01-28 09:27:54 +0100

    improved error handling

--- app.js
+++ app.js
@@ -22,75 +22,94 @@ if(!JASPER_USERNAME || !JASPER_APIKEY || !JASPER_HOSTNAME) {
 }
 
 (async () => {
-  const { devices } = await jasper.devices();
-  const devicesDetails = {};
-
-  if(devices) {
-    devices.forEach(basicInfo => {
-      const { iccid, status, ratePlan, communicationPlan } = basicInfo;
-      devicesDetails[iccid] = {
-        iccid,
-        status,
-        ratePlan,
-        communicationPlan,
-      }
-    });
-  }
+  try {
+    const { devices } = await jasper.devices();
+    const devicesDetails = {};
   
-  const allIccids = Object.keys(devicesDetails);
-  const detailsPromises = allIccids.map(id => jasper.deviceInfo(id));
-  const allDetails = await Promise.all(detailsPromises);
-
-  allDetails.forEach(details => {
-    devicesDetails[details.iccid] = {
-      ...devicesDetails[details.iccid],
-      ...details
-    };
-  });
-
-  const fieldOfInterest = [
-    'iccid',
-    'status',
-    'ratePlan',
-    'communicationPlan',
-    'imsi',
-    'imei',
-    'customer',
-    'accountCustom1',
-    'deviceID',
-    'modemID',
-
-    // sessionInfo
-    'ipAddress',
-    'dateSessionStarted',
-    'dateSessionEnded',
-
-    // ctdDataUsage
-    'ctdDataUsage',
-    'overageLimitReached',
-  ];
-
-  // remove fields of no interest.
-  // TODO: Make configurable/args aware.
-  Object.keys(devicesDetails).forEach(dev => {
-    Object.keys(devicesDetails[dev]).forEach(key => {
-      if(!fieldOfInterest.includes(key)) {
-        delete devicesDetails[dev][key];
+    if(devices) {
+      devices.forEach(basicInfo => {
+        const { iccid, status, ratePlan, communicationPlan } = basicInfo;
+        devicesDetails[iccid] = {
+          iccid,
+          status,
+          ratePlan,
+          communicationPlan,
+        }
+      });
+    }
+    
+    const allIccids = Object.keys(devicesDetails);
+    const detailsPromises = allIccids.map(id => jasper.deviceInfo(id));
+    const allDetails = await Promise.all(detailsPromises);
+  
+    allDetails.forEach(details => {
+      devicesDetails[details.iccid] = {
+        ...devicesDetails[details.iccid],
+        ...details
+      };
+    });
+  
+    const fieldOfInterest = [
+      'iccid',
+      'status',
+      'ratePlan',
+      'communicationPlan',
+      'imsi',
+      'imei',
+      'customer',
+      'accountCustom1',
+      'deviceID',
+      'modemID',
+  
+      // sessionInfo
+      'ipAddress',
+      'dateSessionStarted',
+      'dateSessionEnded',
+  
+      // ctdDataUsage
+      'ctdDataUsage',
+      'overageLimitReached',
+    ];
+  
+    // remove fields of no interest.
+    // TODO: Make configurable/args aware.
+    Object.keys(devicesDetails).forEach(dev => {
+      Object.keys(devicesDetails[dev]).forEach(key => {
+        if(!fieldOfInterest.includes(key)) {
+          delete devicesDetails[dev][key];
+        }
+      })
+    });
+  
+    const columnifyArray = [];
+    Object.keys(devicesDetails).forEach(e => { columnifyArray.push(devicesDetails[e]) });
+  
+    const outputText = columnify(columnifyArray);
+    var o = outputText.split("\n");
+    const headerText = o.shift();
+  
+    o = o.map(m => { return colors.colorize(m) } );
+    const detailsText = o.join("\n");
+  
+    console.log(colors.constants.FgCyan);
+    console.log(headerText + colors.constants.Reset);
+    console.log(detailsText);  
+  }
+  catch(e) {
+    if(e.errno) {
+      if(e.errno == 'ENOTFOUND') {
+      console.error('Unable to get data from Jasper. Please check your Internet connection!');
+      console.error(`(${e.toString()})`);
+      process.exit(-2);
       }
-    })
-  });
-
-  const columnifyArray = [];
-  Object.keys(devicesDetails).forEach(e => { columnifyArray.push(devicesDetails[e]) });
-
-  const outputText = columnify(columnifyArray);
-  var o = outputText.split("\n");
-  const headerText = o.shift();
-
-  o = o.map(m => { return colors.colorize(m) } );
-  const detailsText = o.join("\n");
-
-  console.log(colors.constants.FgCyan);
-  console.log(headerText + colors.constants.Reset);
-  console.log(detailsText);  
+      else if(e.errno == 'ACCESS_DENIED') {
+        console.error('Unable to get data from Jasper. Please check your JASPER_USERNAME, JASPER_APIKEY and JASPER_HOSTNAME environment variables.')
+        process.exit(-3);
+      }
+    }
+    else {
+      console.error(e.toString());
+      process.exit(-999);
+    }
+  }  
 })()
